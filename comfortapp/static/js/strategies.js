2faea12a934e235cacaac8818770886c1a4c58dc
Julio Gabriel Chilela <juliochilela@hotmail.com>
2016-07-25 10:11:21 -0500

    strategy 2 loading

--- comfortapp/static/js/strategies.js
+++ comfortapp/static/js/strategies.js
@@ -12,7 +12,7 @@ changebarstatus();
 
 
 
-//PROGRESS BAR
+//PROGRESS BAR STRATEGY 1
 var progressbar_value = "10";
 
 function changebarstatus(){ 
@@ -57,6 +57,63 @@ function changebarstatus(){
 
         set_values();
 }
+
+
+
+
+
+"""PROGRESS BAR FOR STRATEGY 2"""
+var progressbar_value = "10";
+
+function changebarstatus_st2(){ 
+    //document.getElementById("submit_Value").disabled = true;
+
+    $(function() {
+      var conn = new SockJS('http://172.26.50.120:9081/push');
+
+      conn.onmessage = function(e) {
+        console.log('Got', e.data);
+        var h = document.createElement("H1") 
+        var t = document.createTextNode(e.data);
+        h.appendChild(t);
+        progressbar_value =h.innerHTML;
+        //document.body.appendChild(h);
+        //document.getElementById("test").value = document.body.innerHTML
+
+        //alert(progressbar_value);
+        //localStorage.setItem('loading',  );
+        if (progressbar_value < 0){
+             document.getElementById("myBar").style.width = (String(0)).concat('%');
+            document.getElementById("label").innerHTML = (String(0)).concat('%') ;   
+
+        }
+       else{
+             document.getElementById("myBar").style.width = (String(progressbar_value)).concat('%');
+        document.getElementById("label").innerHTML = (String(progressbar_value)).concat('%') ;
+        }
+
+        //Only perform tasks if this condition is fulfilled
+        if ((String(progressbar_value)).concat('%') == "100%"){
+        document.getElementById("submit_Value").disabled = false;
+           
+           
+        }
+        else{
+        //document.getElementById("submit_Value").disabled = true;
+        }
+        
+      }
+    });
+
+        set_values();
+}
+
+
+
+
+
+
+
 
 function set_values(){
 
